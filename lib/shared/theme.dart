872028d4a71a51367908b9657851a000c34ee3c3
guakoko <gua.koko1@gmail.com>
2021-11-01 09:18:08 +0700

    fetch shared.dart & theme.dart

---
+++ lib/shared/theme.dart
@@ -0,0 +1,44 @@
+part of 'shared.dart';
+
+Color primaryColor = const Color(0xFF6C5ECF);
+Color secondaryColor = const Color(0xFF38ABBE);
+Color alertColor = const Color(0xFFED6363);
+Color priceColor = const Color(0xFF2C96F1);
+Color bgColor1 = const Color(0xFF1F1D2B);
+Color bgColor2 = const Color(0xFF2B2937);
+Color bgColor3 = const Color(0xFF242231);
+Color whiteColor = const Color(0xFFF1F0F2);
+Color blackColor = const Color(0xFF2E2E2E);
+Color greyColor = const Color(0xFF999999);
+
+TextStyle whiteTextStyle1 = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 24,
+);
+
+TextStyle whiteTextStyle2 = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 18,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 14,
+);
+
+const double defaultMargin = 30;
